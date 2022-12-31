2e21f330ea112af3cf20bb7cbf31731a08bcee65
priyan2115 <priyankaa2115@gmail.com>
2022-12-31 18:33:48 -0500

    add modified add.js file

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,6 +2,7 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
 //create new function.
 function buildTable(data){
     //clear out any existing data
@@ -22,6 +23,22 @@ cell.text(val);
 
 });
 }
+// create new function for button click
+function handleClick(){
+    //grab datetime value from filter
+    let date=ds.select("#datetime").property("value");
+    let filterData=tableData;
+    //if statment We’re applying a filter to our data based on a date value.
+    if(date){
+       filterData=filterData.filter(row=>row.datetime===date);
+    };
+    //call buildTable function with filtered data
+    buildTable(filteredData);
+}
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
 
+//build the table when page loads
+buildTable(tableData);
 
 
